7bca0f4131face4455a487b9dc49a83412ac944d
leoav <leoavfag@gmail.com>
2019-11-30 16:59:48 -0300

    Mecânica de movimento
    O personagem se movimenta em 8D :D

---
+++ Tarefas/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float velocidade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizonta = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 position = rb.position;
+
+        position.x = position.x + velocidade * horizonta * Time.deltaTime;
+        position.y = position.y + velocidade * vertical * Time.deltaTime;
+
+        rb.MovePosition(position);
+    }
+}
